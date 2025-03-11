6f2c6a34757ed1bec2869f6a99bccadedd598375
edmond <edmondwu1022@gmail.com>
2025-03-11 15:36:26 +0800

    cheapter5: Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Profile from "./pages/Profile.jsx"
 import About from "./pages/Abouts.jsx"
 import SignIn from "./pages/SignIn.jsx"
 import SignOut from "./pages/SignOut.jsx"
+import Header from "./Components/Header.jsx"
 
 import { BrowserRouter, Route, Routes } from "react-router"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import { FaSearch } from "react-icons/fa"
+import { Link } from "react-router"
+
+export default function Header() {
+
+    return (
+        <header className="bg-slate-200 shadow-md p-5">
+            <div className="flex justify-between items-center max-w-4xl max-h-5 mx-auto">
+                <Link to={"/"}>
+                    <h1 className="text-xl sm:text-md font-bold flex flex-wrap">
+                        <span className="text-blue-800">Digital</span>
+                        <span className="text-cyan-500">Creation</span>
+                    </h1>
+                </Link>
+                <form className="bg-slate-100 rounded-lg flex items-center p-3 sm:p-1">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="bg-transparent text-sm ml-3 focus:outline-none focus:py-1 w-20 sm:w-40 ease-initial duration-200"
+                    />
+                    <FaSearch className="text-slate-600 mr-3" />
+                </form>
+                <div >
+                    <ul className="flex gap-4 justify-between items-center flex-wrap uppercase text-sm">
+                        <Link to={"/"}>
+                            <li className="hidden sm:inline font-normal text-slate-600 py-3 cursor-pointer hover:border-b-1 hover:text-slate-700 hover:font-black">Home</li>
+                        </Link>
+                        <Link to={"/about"}>
+                            <li className="hidden sm:inline font-normal text-slate-600 py-3 cursor-pointer hover:border-b-1 hover:text-slate-700 hover:font-black">About</li>
+                        </Link>
+                        <Link to={"/sign-in"}>
+                            <li className="font-normal text-slate-600 py-3 cursor-pointer hover:border-b-1 hover:text-slate-700 hover:font-black">Sign in</li>
+                        </Link>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
