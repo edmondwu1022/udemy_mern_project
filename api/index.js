a8df77ca9c7af856df1c307e282ad028009725a9
edmond <edmondwu1022@gmail.com>
2025-03-11 18:00:56 +0800

    Chapter7: connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express()
+const url = process.env.MONGO
+
+mongoose.connect(url).then(() => {
+    console.log("connect to Mango DB");
+}).catch(() => {
+    console.log("fail to connect to Mango DB");
+})
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
