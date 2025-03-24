9f6483b836f42ebc76ec8bd499717cb7635eec40
edmond <edmondwu1022@gmail.com>
2025-03-24 14:31:01 +0800

    chatpet26: Add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/user.route.js"
 import authRoute from "./routes/auth.route.js"
+import listingRoute from "./routes/listing.route.js"
 import cookieParser from 'cookie-parser';
 
 const app = express()
@@ -21,6 +22,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoute)
 app.use("/api/auth", authRoute)
+app.use("/api/listing", listingRoute)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
